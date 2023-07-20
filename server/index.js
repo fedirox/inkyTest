4637c27e2de3bdac36e03c1e07f576304d289387
Wiem Essalah <medfedi.belakhel@esprit.tn>
2023-07-20 09:41:44 +0200

    commit after self code review

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
+// Import required modules
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 
-// express app
+// Create the Express app
 const app = express();
 
+// Enable Cross-Origin Resource Sharing for all routes
 app.use(cors());
 
+// Parse incoming request bodies in JSON format
 app.use(express.json());
 
-// open the database
+// Open the SQLite database
 let db = new sqlite3.Database(
   "./db/inkyfada.db",
   sqlite3.OPEN_READONLY,
@@ -21,7 +24,6 @@ let db = new sqlite3.Database(
   }
 );
 
-//api
 app.get("/", (_, res) => {
   res.send("ok");
 });
@@ -29,8 +31,8 @@ app.get("/", (_, res) => {
 app.get("/inky", (req, res) => {
   console.log(req.query.query);
   const { start_date, end_date } = req.query;
-  console.log({start_date, end_date});
- 
+  console.log({ start_date, end_date });
+
   const sql = `SELECT MAX(eventValue)as maxValue, strftime('%Y-%m-%d', createdAt) AS day ,visitId
    FROM events 
    WHERE eventType=1 
@@ -38,19 +40,21 @@ app.get("/inky", (req, res) => {
    AND day <= ?
    GROUP BY visitId, day`;
   try {
-    db.all(sql, [start_date,end_date], (err, rows) => {
+    db.all(sql, [start_date, end_date], (err, rows) => {
       if (err) return res.json({ status: 300, success: false, error: err });
 
       return res.json({ status: 200, data: rows, success: true });
     });
   } catch (error) {
-    console.log(error);
+    // Handle any errors that occur during the query execution
     return res.json({ status: 400, success: false, error });
   }
 });
 
+// Define a catch-all route for handling 404 errors
 app.use((_, res) => {
   res.send("404");
 });
 
+// Start the server and listen on port 5000
 app.listen(5000, console.log("server started on port 5000"));
